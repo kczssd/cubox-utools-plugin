2fc6ab6c01a3a64fd9386198bd7650c9da7902ff
kczssd <511124893@qq.com>
2021-11-25 20:02:13 +0800

    add:GUI

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
-import {handleFiles,handleImg} from './files.js'
+// import {handleFiles,handleImg} from './files.js'
 import Request from './request.js'
-let list = document.querySelector(".upload-list")
 
-//获取用户输入数据
-utools.onPluginEnter(({code,type,payload})=>{
-    handleInputType(type,payload);
+let title="",description="",folder="",tags=[],type,payload;
+const titleNode = document.querySelector(".title");
+const descriptionNode = document.querySelector(".description");
+const folderNode = document.querySelector(".folder");
+const tagsNode = document.querySelector(".tags");
+const publish = document.querySelector(".publish")
+titleNode.addEventListener("input",function(){
+    title = this.value;
 })
-function handleInputType(type,payload){
+descriptionNode.addEventListener("input",function(){
+    description = this.value;
+})
+folderNode.addEventListener("input",function(){
+    folder = this.value;
+})
+tagsNode.addEventListener("input",function(e){
+    tags[0]=this.value;
+})
+tagsNode.addEventListener("blur",function(e){
+    this.style.width = 18+"vw"
+    let width = this.scrollWidth
+    this.style.width = `calc(${width}px - 3vw)`;
+})
+publish.addEventListener("click",function(){
+    handleInputType(type)
+    console.log(type)
+})
+//获取用户输入数据
+utools.onPluginEnter(({code,type:t,payload:p})=>{
+    type=t;
+    payload=p
+})
+function handleInputType(type){
     // if(type==="files"){
     //     handleFiles(payload[0].name,payload[0].path);
     // }else if(type==="img"){
     //     handleImg(payload);
     // }
     if(type==="regex"){
-        handleURL(payload);
+        handleURL();
     }
     if(type==="over"){
-        handleMemo(payload);
+        handleMemo();
     }
 }
-async function handleURL(url){
-    let data = await Request("url",url);
+async function handleURL(){
+    let data = await Request("url",payload,tags,title,description,folder);
     console.log(data)
 }
-async function handleMemo(content){
-    let data = await Request("memo",content,["words"]);
+async function handleMemo(){
+    let data = await Request("memo",payload,[...tags],title,description,folder);
     console.log(data)
 }
